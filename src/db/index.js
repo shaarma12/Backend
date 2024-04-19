c772f193266b4e32e2e63f6ea4407d854657be25
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-04-19 23:18:50 +0530

    Database Connection

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DBName } from "../constant.js";
+
+const connectDB = async () => {
+    try {
+        const connectionString = await mongoose.connect(`${process.env.MONGODB_URI}/${DBName}`);
+        console.log(`\n MONGOOSE CONNECTED! HOST :- ", ${connectionString.connection.host}`);
+    } catch (error) {
+        console.log("Error:- ", error);
+        throw error;
+    }
+}
+export default connectDB;
