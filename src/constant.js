3fc0f6b78ecad1a5d80b51dfd8d24c918158c673
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-04-21 23:56:34 +0530

    adding Configuration && Creating Error Format

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,41 @@
 export const DBName = "YoutubeDB";
+export const Data = [{
+    College_NAME: "Indian Institute of Technology - Delhi",
+    Address: "Hauz Khas, South Delhi",
+    Highest_Package:"2.2 Crore"
+},
+{
+    College_NAME: "Netaji Subhash Institute of Technology",
+    Address: "Azad Hind Fauj Marg, Dwarka Sector-3, Dwarka, Delhi",
+    Highest_Package:"1.25 Crore"
+    },
+    {
+        College_NAME: "Delhi Technological University",
+        Address: " Shahbad Daulatpur, Main Bawana Road, Delhi-110042",
+        Highest_Package:"1.2 Crore"
+    },
+    {
+        College_NAME: "Indian Institute of Information Technology - Delhi",
+        Address: "Okhla Industrial Estate, Phase III, near Govind Puri Metro Station, New Delhi, Delhi 110020",
+        Highest_Package:"65.13 LPA"
+    },
+    {
+        College_NAME: "National Institute of Technology - Delhi",
+        Address: "R48J+6V, National Institute of Technology Plot No. FA7,Zone, P1, GT Karnal Rd, Delhi, 110036",
+        Highest_Package:"55 LPA"
+    },
+    {
+        College_NAME: "Maharaja Agrasen Institute Of Technology",
+        Address: " PSP Area, Plot No-1, Sector-22, Rohini, Delhi-110086",
+        Highest_Package:"57 LPA"
+    },
+    {
+        College_NAME: "Maharaja Surajmal Institute of Technology",
+        Address: "C-4 Janak Puri, New Delhi-110058",
+        Highest_Package:"80 LPA"
+    },
+    {
+        College_NAME: "Bharati Vidyapeeth's College of Engineering - Delhi",
+        Address: "A-4 Block, Baba Ramdev Marg, Shiva Enclave, Paschim Vihar, New Delhi, Delhi, 110063",
+        Highest_Package:"35 LPA"
+    }]
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+import { Data } from "./constant.js";
 
 dotenv.config({
     path:"./env"
 })
 
-connectDB();
+connectDB().then(() => {
+    app.on("error", (error) => {
+        console.log("Error", error);
+        throw error;
+    });
+app.listen(process.env.PORT || 8000,() => {
+    try {
+        console.log(`Server is listen on Port:- ${process.env.PORT}`)
+    } catch (error) {
+        console.log("App is not listen to any PORT", error);
+    }
+})
+    app.get('/', (req,res) => {
+        res.send("It is the data regarding TOP Tier-1 Colleges in Delhi and their Locations!!!");
+    })
+    app.get("/v1/api/Tier-1_College/Delhi", (req, res) => {
+        res.json(Data)
+    })
+})
