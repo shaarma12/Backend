f29969f482da193cfd48340872c58e30d726e231
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-04-24 23:59:03 +0530

    returning asyncHandler wrapper

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler = (requestHandler) => {
     (req,res,next) => {
-        Promise.resolve(requestHandler(req,res,next)).catch((err) => {
+       return Promise.resolve(requestHandler(req,res,next)).catch((err) => {
             next(err)
         })
     }
